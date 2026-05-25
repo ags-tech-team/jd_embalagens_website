46915b1fd0ae21f1cfcb1e4fe3f0b6869185ad7a
Arthur Ribeiro Goncalves <ribeirogon.arthur@gmail.com>
2026-05-25 09:17:20 -0300

    arrumando imagens e pdf

--- src/pages/Products/data.ts
+++ src/pages/Products/data.ts
@@ -3,83 +3,83 @@ export const productsData = {
     title: "JD Color Copos",
     icon: "🥤",
     items: [
-      { id: 1, name: "Copo PP 330ml", image: "products_optmized/copoPP330.png" },
-      { id: 2, name: "Copo PP 440ml", image: "products_optmized/copoPP440.png" },
-      { id: 3, name: "Copo PP 550ml", image: "products_optmized/copoPP550.png" },
-      { id: 4, name: "Copo PP 770ml", image: "products_optmized/copoPP770.png" },
-      { id: 5, name: "Copo de Papel", image: "products_optmized/copoDePapel.png" },
-      { id: 6, name: "Copo de Isopor", image: "products_optmized/copoIsopor.png" },
-      { id: 7, name: "Copo Long Drink", image: "products_optmized/copoLongDrink.png" },
-      { id: 8, name: "Copo Eco", image: "products_optmized/copoEco.png" },
-      { id: 9, name: "Copo Twister", image: "products_optmized/copoTwister.png" }
+      { id: 1, name: "Copo PP 330ml", image: "products_optimized/copoPP330.webp" },
+      { id: 2, name: "Copo PP 440ml", image: "products_optimized/copoPP440.webp" },
+      { id: 3, name: "Copo PP 550ml", image: "products_optimized/copoPP550.webp" },
+      { id: 4, name: "Copo PP 770ml", image: "products_optimized/copoPP770.webp" },
+      { id: 5, name: "Copo de Papel", image: "products_optimized/copoDePapel.webp" },
+      { id: 6, name: "Copo de Isopor", image: "products_optimized/copoIsopor.webp" },
+      { id: 7, name: "Copo Long Drink", image: "products_optimized/copoLongDrink.webp" },
+      { id: 8, name: "Copo Eco", image: "products_optimized/copoEco.webp" },
+      { id: 9, name: "Copo Twister", image: "products_optimized/copoTwister.webp" }
     ]
   },
   potes: {
-    title: "JD Color Potes",
+    title: "JD Potes",
     icon: "🍨",
     items: [
-      { id: 11, name: "Pote PET 250ml", image: "products_optmized/potePet250.png" },
-      { id: 12, name: "Pote PET 500ml", image: "products_optmized/potePet500.png" },
-      { id: 13, name: "Pote de Papel", image: "products_optmized/potePapel.png" },
-      { id: 14, name: "Pote de Isopor", image: "products_optmized/poteIsopor.png" }
+      { id: 11, name: "Pote PET 250ml", image: "products_optimized/potePet250.webp" },
+      { id: 12, name: "Pote PET 500ml", image: "products_optimized/potePet500.webp" },
+      { id: 13, name: "Pote de Papel", image: "products_optimized/potePapel.webp" },
+      { id: 14, name: "Pote de Isopor", image: "products_optimized/poteIsopor.webp" }
     ]
   },
   sacolas: {
-    title: "JD Color Sacolas",
+    title: "JD Sacolas",
     icon: "🛍️",
     items: [
-      { id: 15, name: "Saco pra Lanche", image: "products_optmized/sacoLanche.png" },
-      { id: 16, name: "Saco de Delivery", image: "products_optmized/sacoDelivery.png" },
-      { id: 17, name: "Saco para Pães", image: "products_optmized/sacoPaes.png" },
-      { id: 20, name: "Sacola de Papel", image: "products_optmized/sacolaPapel.png" },
-      { id: 33, name: "Sacola de Boca Palhaço", image: "products_optmized/sacolabocapalhaco.png" }
+      { id: 15, name: "Saco pra Lanche", image: "products_optimized/sacoLanche.webp" },
+      { id: 16, name: "Saco de Delivery", image: "products_optimized/sacoDelivery.webp" },
+      { id: 17, name: "Saco para Pães", image: "products_optimized/sacoPaes.webp" },
+      { id: 20, name: "Sacola de Papel", image: "products_optimized/sacolaPapel.webp" },
+      { id: 33, name: "Sacola de Boca Palhaço", image: "products_optimized/sacolabocapalhaco.webp" }
     ]
   },
   caixas: {
-    title: "JD Color Caixas",
+    title: "JD Caixas",
     icon: "📦",
     items: [
-      { id: 18, name: "Caixa de Pizza", image: "products_optmized/caixaPizza.png" },
-      { id: 19, name: "Caixa de Salgado", image: "products_optmized/caixaSalgado.png" },
-      { id: 32, name: "Caixa PET para Salgados", image: "products_optmized/embalagempet.png" }
+      { id: 18, name: "Caixa de Pizza", image: "products_optimized/caixaPizza.webp" },
+      { id: 19, name: "Caixa de Salgado", image: "products_optimized/caixaSalgado.webp" },
+      { id: 32, name: "Caixa PET para Salgados", image: "products_optimized/embalagempet.webp" }
     ]
   },
   isopor: {
-    title: "JD Color Isopor",
+    title: "JD Isopor",
     icon: "🧊",
     items: [
-      { id: 24, name: "Marmita de Isopor", image: "products_optmized/marmitaIsopor.png" },
-      { id: 34, name: "Embalagem para Hambúrguer (Quadrada)", image: "products_optmized/hamburguerQuadrada.png" },
-      { id: 35, name: "Embalagem para Hambúrguer (Redonda)", image: "products_optmized/hamburguerredonda.jpeg" }
+      { id: 24, name: "Marmita de Isopor", image: "products_optimized/marmitaIsopor.webp" },
+      { id: 34, name: "Embalagem para Hambúrguer (Quadrada)", image: "products_optimized/hamburguerquadrada.webp" },
+      { id: 35, name: "Embalagem para Hambúrguer (Redonda)", image: "products_optimized/hamburguerredonda.webp" }
     ]
   },
   outros: {
-    title: "JD Color Produtos para Automoção",
+    title: "JD Produtos para Automoção",
     icon: "📦",
     items: [
-      { id: 22, name: "Etiqueta de Preço", image: "products_optmized/EtiquetaPreço.png" },
-      { id: 36, name: "Etiquetas de Balança", image: "products_optmized/rotulodebalanca.png" },
-      { id: 37, name: "Rótulos", image: "products_optmized/rotulo.png" },
-      { id: 38, name: "Lacres", image: "products_optmized/lacre.jpeg" }
+      { id: 22, name: "Etiqueta de Preço", image: "products_optimized/EtiquetaPreço.webp" },
+      { id: 36, name: "Etiquetas de Balança", image: "products_optimized/rotulodebalanca.webp" },
+      { id: 37, name: "Rótulos", image: "products_optimized/rotulo.webp" },
+      { id: 38, name: "Lacres", image: "products_optimized/lacre.webp" }
     ]
   },
   limpeza: {
-    title: "JD Color Limpeza e Higiene",
+    title: "JD Limpeza e Higiene",
     icon: "🧹",
     items: [
-      { id: 25, name: "Produtos de Limpeza", image: "products_optmized/produtosLimpeza.png" },
-      { id: 26, name: "Saco de Lixo", image: "products_optmized/sacoLixo.png" },
-      { id: 27, name: "Papel Higiênico", image: "products_optmized/papelHigienico.png" },
-      { id: 28, name: "Papel Toalha", image: "products_optmized/roloPapelToalha.png" }
+      { id: 25, name: "Produtos de Limpeza", image: "products_optimized/produtosLimpeza.webp" },
+      { id: 26, name: "Saco de Lixo", image: "products_optimized/sacoLixo.webp" },
+      { id: 27, name: "Papel Higiênico", image: "products_optimized/papelHigienico.webp" },
+      { id: 28, name: "Papel Toalha", image: "products_optimized/roloPapelToalha.webp" }
     ]
   },
   brindes: {
-    title: "JD Color Brindes em Geral",
+    title: "JD Brindes em Geral",
     icon: "🎁",
     items: [
-      { id: 29, name: "Lixeira para Carro", image: "products_optmized/lixeiraCarro.png" },
-      { id: 30, name: "Canetas", image: "products_optmized/canetas.png" },
-      { id: 31, name: "Copo Stanley", image: "products_optmized/copoStanley.png" }
+      { id: 29, name: "Lixeira para Carro", image: "products_optimized/lixeiraCarro.webp" },
+      { id: 30, name: "Canetas", image: "products_optimized/canetas.webp" },
+      { id: 31, name: "Copo Stanley", image: "products_optimized/copoStanley.webp" }
     ]
   }
 };
